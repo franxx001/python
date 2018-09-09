46980f5d0b3352611eaa6fd72e18d445edce57e5
geal <chengeal@outlook.com>
2018-09-09 23:42:33 +0800

    控制流

---
+++ 控制流/break.py
@@ -0,0 +1,6 @@
+while True:
+    s = input('enter something:')
+    if s == 'quit':
+        break
+    print('length is the string is ', len(s))
+print('done')
---
+++ 控制流/for.py
@@ -0,0 +1,9 @@
+for i in range(1, 5):
+    print(i)
+else:
+    print('the for loop is over')
+# 注意 range函数
+for i in range(1, 10, 2):
+    print(i)
+else:
+    print('the for loop is over')

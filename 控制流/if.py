9abc9983d4d78a16d96655cb5e0a85da1e17f34f
geal <chengeal@outlook.com>
2018-09-09 17:26:53 +0800

    1

--- 控制流/if.py
+++ 控制流/if.py
@@ -7,5 +7,5 @@ if guess == number:
 elif guess < number:
     print('no,it is a little higher than that')
 else:
-    print('no,is is  little lower than that')
+    print('no,is is a little lower than that')
 print('Done')
